574a8dfd93cebbf05b74d0629e1c759c467f2aba
vit01 <me@ii-net.tk>
2021-04-07 00:08:22 +0800

    add pagination to telegram bot

--- csapi.py
+++ csapi.py
@@ -3,11 +3,11 @@ import requests
 import math
 import pandas as pd
 from dateutil import parser
+import traceback
 
-def query_clearspending(base_url):
-    print("Запрашиваем 1 страницу")
+def query_clearspending_page(base_url, page=1, perpage=10):
     results = []
-    f = requests.get(base_url)
+    f = requests.get(base_url + "&page={0}&perpage={1}&sort=signDate".format(page, perpage))
     try:
         contents = json.loads(f.text)
     except:
@@ -18,16 +18,25 @@ def query_clearspending(base_url):
     total = contents["contracts"]["total"]
     perpage = contents["contracts"]["perpage"]
     pages = math.ceil(total / perpage)
-    
-    print("Всего", total, "контрактов")
-    if pages >= 2:
-        for p in range(2, pages+1):
-            print("Страница {0}/{1}".format(p, pages))
-            f = requests.get(base_url + "&page=" + str(p))
-            contents = json.loads(f.text)
-            results.extend(contents["contracts"]["data"])
             
+    return results, {"total": total, "perpage": perpage, "pages": pages}
+
+def query_clearspending(base_url):
+    results, meta = query_clearspending_page(base_url, perpage=50)
+    if results == None:
+        return None
+    
+    print("Всего", meta["total"], "контрактов")
+    if meta["pages"] >= 2:
+        for p in range(2, pages+1):
+            print("Страница {0}/{1}".format(p, meta["pages"]))
+            res2, meta2 = query_clearspending_page(base_url, p, meta["perpage"])
+            if res2 == None:
+                break
+            
+            results.extend(res2)
     return results
+
 
 def pandas_ds(contracts):
     ds = []
@@ -36,9 +45,21 @@ def pandas_ds(contracts):
     for contract in contracts:
         if (contract.get("price") == None):
             continue
+        customer = contract.get("customer")
+        if customer != None:
+            customer = customer.get("fullName")
+            
+        products_arr = contract.get("products")
+        products = []
+        for p in products_arr:
+            if not "name" in p.keys():
+                products.append(str(p.get("additionalInfo")))
+            else:
+                products.append(str(p.get("name")))
+        
         ds.append((parser.parse(contract.get("publishDate")), contract.get("fz"), math.floor(contract.get("price")),\
-                   ",".join((str(p.get("name")) for p in contract["products"])),\
-                   contract["customer"]["fullName"], contract.get("printFormUrl")))
+                   ",".join(products),\
+                   customer, contract.get("printFormUrl")))
 
     ds.sort(key = lambda row: row[0].timestamp(), reverse=True)
     #print(titles, "\n")
@@ -53,7 +74,24 @@ def tg_formatting(pd_ds):
     keys = pd_ds.keys()
     for row in pd_ds.values[0:3]:
         for key, i in zip(keys, range(len(keys))):
-            txt += "<b>{0}:</b> {1}\n".format(key, escape_markup(row[i]))
+            row_escaped = escape_markup(row[i])
+            if len(row_escaped) > 250:
+                row_escaped = row_escaped[0:250]
+            txt += "<b>{0}:</b> {1}\n".format(key, row_escaped)
         txt+="\n\n"
 
     return txt
+
+def query_clearspending_text(base_url, page=1, perpage=10):
+    try:
+        result, meta = query_clearspending_page(base_url, page, perpage)
+        if result == None:
+            return "clearspending api выдало пустой ответ", None
+        
+        pd_ds = pandas_ds(result)
+        text_to_send = tg_formatting(pd_ds)
+        return text_to_send, meta
+
+    except Exception as e:
+        traceback.print_exc()
+        return str(e), None
--- tgbot.py
+++ tgbot.py
@@ -6,6 +6,8 @@ import sys
 import csapi
 import telebot
 from telebot import util
+from telebot.types import InlineKeyboardButton
+from telegram_bot_pagination import InlineKeyboardPaginator
 import traceback
 import json
 
@@ -13,28 +15,39 @@ config = json.load(open("bot_config.json"))
 bot = telebot.TeleBot(config["token"])
 base_url = "http://openapi.clearspending.ru/restapi/v3/"
 
+last_queries = {}
+msgs_to_delete = {}
+
+def set_last_query(message, value):
+    if value == None:
+        if str(message.chat.id) in last_queries.keys():
+            del last_queries[str(message.chat.id)]
+    else:
+        last_queries[str(message.chat.id)] = value
+
+def add_to_delete_buffer(message):
+    if not str(message.chat.id) in msgs_to_delete.keys():
+        msgs_to_delete[str(message.chat.id)] = []
+    
+    msgs_to_delete[str(message.chat.id)].append(message.id)
+
 @bot.message_handler(commands=['start', 'help'])
 def start(message):
     bot.send_message(message.chat.id, "Поиск по закупкам")
 
 @bot.message_handler(commands=['products'])
 def products_search(message):
-    kw_str = "+".join(message.text.split(" ")[1:])
-    url = base_url + "contracts/search/?productsearch=" + kw_str
+    keys = [x.strip() for x in message.text.split(" ")[1:]]
+    keys = [x for x in keys if x != ""]
     
-    try:
-        result = csapi.query_clearspending(url)
-        if result == None:
-            bot.send_message(message.chat.id, "clearspending api выдало пустой ответ")
-            
-        pd_ds = csapi.pandas_ds(result)
-        text_to_send = csapi.tg_formatting(pd_ds)
-        pieces = util.split_string(text_to_send, 3000)
-        for piece in pieces:
-            bot.send_message(message.chat.id, piece, parse_mode="HTML")
-    except Exception as e:
-        traceback.print_exc()
-        bot.send_message(message.chat.id, str(e))
+    if len(keys) == 0:
+        bot.send_message(message.chat.id, "Пустой запрос. Вводите ключевые слова в формате /команда слово слово")
+        return
+    
+    kw_str = "+".join(keys)
+    url = base_url + "contracts/search/?productsearch=" + kw_str
+    set_last_query(message, url)
+    send_character_page(message)
 
 @bot.message_handler(commands=['customers'])
 def customers_search(message):
@@ -49,6 +62,51 @@ def suppliers_search(message):
     url= base_url + "suppliers/search/?namesearch=" + kw_str
     
     bot.send_message(message.chat.id, url, parse_mode="HTML")
+
+@bot.callback_query_handler(func=lambda call: call.data.split('#')[0]=='character')
+def characters_page_callback(call):
+    page = int(call.data.split('#')[1])
+    
+    add_to_delete_buffer(call.message)
+    for mid in msgs_to_delete[str(call.message.chat.id)]:
+        bot.delete_message(call.message.chat.id, mid)
+    msgs_to_delete[str(call.message.chat.id)] = []
+    
+    send_character_page(call.message, page)
+
+@bot.callback_query_handler(func=lambda call: call.data=='back')
+def characters_go_back(call):
+    set_last_query(call.message, None)
+    msgs_to_delete[call.message.chat.id] = []
+    
+    bot.send_message(call.message.chat.id, "Введите новый запрос", parse_mode="HTML")
+
+def send_character_page(message, page=1):
+    url = last_queries[str(message.chat.id)]
+    result, meta = csapi.query_clearspending_text(url, page)
+    if meta == None or meta["pages"] == 1:
+        set_last_query(message, None)
+        bot.send_message(message.chat.id, result, parse_mode="HTML")
+        return
+
+    pieces = util.split_string(result, 3000)
+    if len(pieces) > 1:
+        for piece in pieces[:len(pieces)-2]:
+            msg = bot.send_message(message.chat.id, piece, parse_mode="HTML")
+            add_to_delete_buffer(msg)
+
+    paginator = InlineKeyboardPaginator(
+        meta["pages"],
+        current_page=page,
+        data_pattern='character#{page}')
+
+    paginator.add_after(InlineKeyboardButton('Отмена', callback_data='back'))
+
+    bot.send_message(
+        message.chat.id,
+        pieces[-1],
+        reply_markup=paginator.markup,
+        parse_mode='HTML')
 
 def telegram_exception(e):
     print(str(e))
